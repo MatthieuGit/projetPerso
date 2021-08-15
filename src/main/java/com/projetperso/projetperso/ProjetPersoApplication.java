41b0aecedaa0d66013f534cebdcdcc706a96b115
matthieu.bailly <ext-matthieu.bailly@flowbird.group>
2021-08-15 17:43:04 +0200

    first commit - init project

---
+++ src/main/java/com/projetperso/projetperso/ProjetPersoApplication.java
@@ -0,0 +1,13 @@
+package com.projetperso.projetperso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPersoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetPersoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetperso/projetperso/ProjetPersoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetperso.projetperso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPersoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
